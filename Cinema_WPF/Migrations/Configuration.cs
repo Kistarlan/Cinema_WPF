5a1139f6903bb59eef4b3d5f216e61ee68cb86ac
vletnyanchyn <vletnyanchyn@cybervisiontech.com>
2017-10-12 15:14:11 +0300

    Add default data in db

--- Cinema_WPF/Migrations/Configuration.cs
+++ Cinema_WPF/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace Cinema_WPF.Migrations
 {
+    using Cinema_WPF.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -15,6 +16,21 @@ namespace Cinema_WPF.Migrations
 
         protected override void Seed(Cinema_WPF.Context.CinemaContext context)
         {
+
+            var AdminRole = new UserRole() { Name = "Admin" };
+            context.UserRoles.Add(AdminRole);
+            context.SaveChanges();
+            var CashierRole = new UserRole() { Name = "Cashier" };
+
+            context.UserRoles.Add(CashierRole);
+
+            context.SaveChanges();
+            User AdminUser = new User() { Login = "Admin", Name = "Admin", Surname = "Admin", Password = "Admin", UserRole = context.UserRoles.Find(AdminRole), UserRoleId = 0, DateOfBirth = Convert.ToDateTime(new DateTime(1997, 3, 12)).Date };
+
+            //UserRole AdminRole = new UserRole() { Name = "Admin" };
+            //UserRole CashierRole = new UserRole() { Name = "Cashier" };
+            //dbContext.UserRoles.Add(AdminRole);
+            //dbContext.UserRoles.Add(CashierRole);
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
